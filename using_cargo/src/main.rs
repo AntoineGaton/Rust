b6ef7dcf0c212f698f5d488b531eeb9ea92a7690
Antoine Gaton <antoinegaton@gmail.com>
2022-09-09 20:13:27 -0400

    09.09.22

---
+++ using_cargo/src/main.rs
@@ -0,0 +1,6 @@
+// TO INITIALIZE NEW RUST PROJECT USING CARGO RUN...cargo new NAME_OF_PROJECT
+// TO COMPILE CODE USE...cargo build
+// TO RUN .exe cd in \target\debug then .\using_cargo.exe
+fn main() {
+    println!("Hello, world!");
+}
